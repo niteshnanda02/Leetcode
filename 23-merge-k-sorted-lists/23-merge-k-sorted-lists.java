33f2d15b23c69cdc3c0030d7b29f2a29e06539f9
Nitesh <40588354+niteshnanda02@users.noreply.github.com>
2022-02-05 14:20:55 +0530

    Time: 3 ms (88.39%), Space: 47.3 MB (8.81%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,32 +10,35 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(new Comparator<>(){
-            @Override
-            public int compare(ListNode l1,ListNode l2){
-                return l1.val-l2.val;
-            }
-        });
+        if(lists == null || lists.length == 0)
+            return null;
+        int n = lists.length;
+        return partition(lists,0,n-1);
+    }
+    
+    ListNode partition(ListNode[] list, int s, int e){
+        if(s==e)
+            return list[s];
+        int mid = s+(e-s)/2;
+        ListNode left = partition(list, s, mid);
+        ListNode right = partition(list, mid+1, e);
         
-        for(ListNode node: lists){
-            if(node!=null)
-                minHeap.add(node);
+        return merge(left,right);
+    }
+    
+    ListNode merge(ListNode left, ListNode right){
+        if(left == null)
+            return right;
+        if(right == null)
+            return left;
+        
+        if(left.val<right.val){
+            left.next = merge(left.next, right);
+            return left;
+        }else{
+            right.next = merge(left, right.next);
+            return right;
         }
-        
-        ListNode head = null,tail = null;
-        while(!minHeap.isEmpty()){
-            ListNode cc = minHeap.remove();
-            if(head == null){
-                head = cc;
-                tail = cc;
-            }else{
-                tail.next = cc;
-                tail = cc;
-            }
-            if(cc.next!=null)
-                minHeap.add(cc.next);
-        }
-        
-        return head;
+            
     }
 }
