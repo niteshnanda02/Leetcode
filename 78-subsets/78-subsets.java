ce32ef46b0180ce89d1c6b1b4df420cc055a3c4a
Nitesh <40588354+niteshnanda02@users.noreply.github.com>
2022-02-13 14:30:03 +0530

    Time: 2 ms (44.88%), Space: 43.4 MB (13.19%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,19 +1,26 @@
 class Solution {
     List<List<Integer>> list = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> subList= new ArrayList<>();
-        solve(nums,0, subList);
+        List<Integer> subList = new ArrayList<>();
+        solve(nums, 0, subList);
         return list;
     }
-    
-    void solve(int[] nums,int start,List<Integer> subList){
+    void solve(int[] nums, int start, List<Integer> subList){
+        
+        // base case
         if(start == nums.length){
             list.add(new ArrayList<>(subList));
             return;
         }
+        
+        
+        // recursion call
         int cc = nums[start];
+        
+        // either will take this
         subList.add(cc);
         solve(nums, start+1, subList);
+        // or will skip this element
         subList.remove((Integer) cc);
         solve(nums, start+1, subList);
     }
