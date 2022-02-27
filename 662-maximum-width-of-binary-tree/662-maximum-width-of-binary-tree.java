779fb9e9addb321433369240ce18e7a964ce8e4b
Nitesh <40588354+niteshnanda02@users.noreply.github.com>
2022-02-27 13:15:16 +0530

    Time: 3 ms (51.41%), Space: 44.3 MB (8.12%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -19,28 +19,31 @@ class Solution {
         Map<TreeNode, Integer> map = new HashMap<>();
         queue.add(root);
         map.put(root, 0);
-        int max=1;
+        int max = 1;
         while(!queue.isEmpty()){
             int size = queue.size();
-            int start = 0, end=0;
+            int end = 0, start =0;
             for(int i=0;i<size;i++){
-                TreeNode elem = queue.remove();
+                TreeNode rm = queue.remove();
+                int idx = map.get(rm);
                 if(i==0)
-                    start = map.get(elem);
+                    start = idx;
                 if(i==size-1)
-                    end = map.get(elem);
-                if(elem.left!=null){
-                    queue.add(elem.left);
-                    map.put(elem.left, map.get(elem)*2+1);
+                    end = idx;
+                
+                if(rm.left!=null){
+                    queue.add(rm.left);
+                    map.put(rm.left, 2*idx+1);
                 }
-                if(elem.right!=null){
-                    queue.add(elem.right);
-                    map.put(elem.right, map.get(elem)*2+2);
+                
+                if(rm.right!=null){
+                    queue.add(rm.right);
+                    map.put(rm.right, 2*idx+2);
                 }
             }
-            int diff = end - start+1;
-            max = Math.max(max, diff);
+            max=Math.max(max, end-start+1);
         }
+        
         return max;
     }
 }
