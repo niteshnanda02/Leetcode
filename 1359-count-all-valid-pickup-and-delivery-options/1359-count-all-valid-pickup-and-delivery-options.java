67b6e5e7cfafefcdb1dbe99e2e99b3deace6ad78
Nitesh <40588354+niteshnanda02@users.noreply.github.com>
2022-03-06 21:04:26 +0530

    Time: 0 ms (100.00%), Space: 39 MB (58.64%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,13 +1,15 @@
 class Solution {
     public int countOrders(int n) {
-        long mod = (long) Math.pow(10, 9)+7;
+        long mod = (long) Math.pow(10, 9) +7;
         long res = 1;
+        
         for(int i=1;i<=n;i++){
-            res *=i;
+            res*=i;
             res%=mod;
-            res*=2*i-1;
+            res*=(2*i-1);
             res%=mod;
         }
+        
         return (int) res;
     }
 }
