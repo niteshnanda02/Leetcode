013c945fba8d835074cd825c7a3a491b971f4fe4
Nitesh <40588354+niteshnanda02@users.noreply.github.com>
2022-05-06 21:55:37 +0530

    Time: 115 ms (19.86%), Space: 49.8 MB (51.97%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,36 +1,35 @@
 class Solution {
     class Node{
         char data;
-        int index;
-        Node(char a, int b){
-            data=a;
-            index=b;
+        int freq;
+        Node(char d, int f){
+            data = d;
+            freq = f;
         }
-        
     }
     public String removeDuplicates(String s, int k) {
         Stack<Node> stack = new Stack<>();
         for(char c: s.toCharArray()){
             if(!stack.isEmpty() && stack.peek().data == c){
                 Node rv = stack.pop();
-                rv.index+=1;
+                rv.freq+=1;
                 stack.push(rv);
             }else{
                 stack.push(new Node(c, 1));
             }
             
-            if(!stack.isEmpty() && stack.peek().index == k)
+            if(!stack.isEmpty() && stack.peek().freq == k)
                 stack.pop();
         }
         
         StringBuilder builder=new StringBuilder();
         while(!stack.isEmpty()){
             Node rv = stack.pop();
-            int ind = rv.index;
-            while(ind-->0){
+            int freq = rv.freq;
+            while(freq-->0)
                 builder.append(rv.data);
-            }
         }
+        
         return builder.reverse().toString();
     }
 }
