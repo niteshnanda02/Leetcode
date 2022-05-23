fd9cb0ec055da602d1b83a1333f849aab7c5461d
Nitesh <40588354+niteshnanda02@users.noreply.github.com>
2022-05-23 20:42:41 +0530

    Time: 299 ms (19.71%), Space: 88.1 MB (27.59%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -2,23 +2,30 @@ class Solution {
     int[][][] dp;
     public int findMaxForm(String[] strs, int m, int n) {
         dp = new int[m+1][n+1][strs.length];
-        return solve(strs, m, n ,0);
+        int[][] cnt = new int[strs.length][2];
+        int i=0;
+        for(String s: strs){
+            int zero = cntZ(s);
+            int one = s.length()-zero;
+            cnt[i][0]=zero;
+            cnt[i][1]=one;
+            i++;
+        }
+        return solve(cnt, m, n ,0);
     }
     
-    int solve(String[] strs, int m, int n, int ind){
+    int solve(int[][] cnt, int m, int n, int ind){
         
-        if(ind == strs.length || m+n==0)
+        if(ind == cnt.length || m+n==0)
             return 0;
         if(dp[m][n][ind]>0)
             return dp[m][n][ind];
-        int zero = cntZ(strs[ind]);
-        int one = strs[ind].length()-zero;
         
         int accept = 0, skip = 0;
-        if(m>=zero && n>=one)
-            accept = solve(strs, m-zero, n-one, ind+1)+1;
+        if(m>=cnt[ind][0] && n>=cnt[ind][1])
+            accept = solve(cnt, m-cnt[ind][0], n-cnt[ind][1], ind+1)+1;
         
-        skip = solve(strs, m, n, ind+1);
+        skip = solve(cnt, m, n, ind+1);
         return dp[m][n][ind]=Math.max(accept, skip);
     }
     
